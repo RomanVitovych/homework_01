91eb7d8966a852edf86f6d812253f9ef114d3182
Boqdan2005 <cvyrydboqdan@gmail.com>
2020-09-10 12:17:45 +0300

    Updates

---
+++ precache-manifest.8f4e2f5046f86bfa303a0369e7ee6627.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b90407333773059346e3d9a7a40920",
+    "url": "/homework_01/index.html"
+  },
+  {
+    "revision": "1982e4d52390de5c990b",
+    "url": "/homework_01/static/css/main.16b9bbd1.chunk.css"
+  },
+  {
+    "revision": "dcea5bc4eb4703125ea3",
+    "url": "/homework_01/static/js/2.a0219544.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homework_01/static/js/2.a0219544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1982e4d52390de5c990b",
+    "url": "/homework_01/static/js/main.a9581827.chunk.js"
+  },
+  {
+    "revision": "dfe312303e96559c3f55",
+    "url": "/homework_01/static/js/runtime-main.58df2047.js"
+  }
+]);
